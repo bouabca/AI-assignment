c1a7dace68944ab21d1a51ef05ccb6b01559eb8d
bouabca <148580325+bouabca@users.noreply.github.com>
2025-04-20 15:50:02 +0100

    final commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'all',
+      // Vous pouvez ajouter d'autres hôtes autorisés si nécessaire
+    ]
+  }
 })
